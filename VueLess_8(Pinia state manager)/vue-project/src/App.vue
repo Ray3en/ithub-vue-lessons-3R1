af18111efa04526b5df0a7ccfe0ea0a3cb09e3ba
ray3en <tigranlee@gmail.com>
2024-02-28 18:33:52 +0300

    28.02

---
+++ VueLess_8(Pinia state manager)/vue-project/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+  <Counter/>
+</template>
---
+++ VueLess_8(Pinia state manager)/vue-project/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Counter</h2>
+        <h3>{{ counterStore.count }}</h3>
+        <div>
+            <p>
+                <button @click="counterStore.decrement">-</button>
+                <button @click="counterStore.increment">+</button>
+            </p>
+            <div>
+                <input v-model="inputValue" type="number"/>
+                <button @click="handle">change value</button>            
+            </div>
+            <button @click="asyncRandom">Async random value</button>
+        </div>
+        <h2>Double value (getters)</h2>
+        <h3>{{ counterStore.doubleValue }}</h3>
+    </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/CounterStore';
+import { ref } from 'vue';
+
+const inputValue = ref('')
+const counterStore = useCounterStore()
+
+function handle(){
+    counterStore.changeValue(+inputValue.value)
+}
+
+function asyncRandom(){
+    counterStore.getRandonValue()
+}
+</script>
+
